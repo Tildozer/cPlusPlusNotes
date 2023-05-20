d99825771cc5199e49ae27cecc7dbdc2eb6582ee
Tildozer <actninswitch@gmail.com>
2023-05-20 10:15:27 -0600

    changed all math stuff to one function for simplicity on main.cpp

--- main.cpp
+++ main.cpp
@@ -10,9 +10,6 @@ int main() {
 // if number has decimals use double
     double sales = 9.99;
     cout << "File size " << file_size << " sales " << sales << " counter " << counter << endl;
-    swap(3, 6);
-    constants();
-    mathematicalExpression(5, 6);
-    expressionToCode(60, 4);
+    doMath();
     return 0;
 }
